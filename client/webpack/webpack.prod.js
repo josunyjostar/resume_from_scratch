570a5e801f3f8314536b584b7b2b11a34a00a2c8
parkjeyoung <dkfmwpsxm@naver.com>
2022-07-21 00:25:17 +0900

    Feat : webpack plugin 설치(analyzer, CopyPlugin)

--- client/webpack/webpack.prod.js
+++ client/webpack/webpack.prod.js
@@ -1,12 +1,14 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
-  mode: 'production',
-  devtool: 'hidden-source-map',
+  mode: "production",
+  devtool: "hidden-source-map",
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.name': JSON.stringify('production'),
+      "process.env.name": JSON.stringify("production"),
     }),
+    new BundleAnalyzerPlugin(),
   ],
 };
 
