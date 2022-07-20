217bb287e6cf74dfbe71259012678ed71210893e
parkjeyoung <dkfmwpsxm@naver.com>
2022-07-20 17:58:56 +0900

    Feat : 웹팩 설정 파일 merge 위한 dev-dependency 추가 (webpack.config.js + dev or production 조합) 및 멀티환경 스크립트 추가(dev,prod)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,7 +11,7 @@ const Container = styled.div`
 export const App = () => {
   return (
     <Container>
-      <h1>React TypeScript</h1>
+      <h1>React TypeScript {process.env.NODE_ENV}</h1>
       <img src={IMAGE} alt="" width="300" height="200"></img>
       <img src={LOGO} alt="" width="300" height="200"></img>
     </Container>
---
+++ client/webpack/webpack.config.js
@@ -0,0 +1,9 @@
+const {merge} = require('webpack-merge');
+const commonConfig = require('webpack.common.js');
+
+module.exports = envVariables => {
+  const {env} = envVariables;
+  const envConfig = require(`./webpack.${env}.js`);
+  const config = merge(commonConfig, envConfig);
+  return config;
+};
