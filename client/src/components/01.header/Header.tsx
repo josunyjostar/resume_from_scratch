449dc4074e121641ec0e441988c124b9742b1010
parkjeyoung <dkfmwpsxm@naver.com>
2022-07-25 20:04:36 +0900

    Feat : header component 작성

--- client/src/components/01.header/Header.tsx
+++ client/src/components/01.header/Header.tsx
@@ -2,10 +2,9 @@ import styled from "styled-components";
 import Inner from "../00.common/Inner";
 import { HeaderProps } from "../components.model";
 
-const Container = styled.div`
-  border: 1px solid red;
+const Container = styled.section`
   h1 {
-    font-size: 25px;
+    font-size: 35px;
     font-weight: bold;
     margin-top: 60px;
     margin-bottom: 30px;
@@ -13,14 +12,17 @@ const Container = styled.div`
 
   @media screen and (max-width: 767px) {
     h1 {
-      font-size: 1px;
-      font-weight: bold;
+      font-size: 18px;
       margin-top: 15px;
       margin-bottom: 10px;
     }
   }
 
   @media (min-width: 768px) and (max-width: 1024px) {
+    h1 {
+      margin-top: 20px;
+      margin-bottom: 15px;
+    }
   }
 `;
 
